ce75257c76d7c1ca041ce1c5be88eb6f1105ca33
Amir Hamdy <Amir.Hamdy@ideainmotion.org>
2019-11-19 19:25:42 +0200

    fix refetching todos

--- src/TodosContainer.js
+++ src/TodosContainer.js
@@ -8,7 +8,7 @@ export const ADD_TODO = gql`mutation createTodo($task: String!)
   {createTodo(input: {task: $task, user_id: "", done: false}) {id task done}}`;
 
 export default () => (
-  <Query query={GET_TODOS}>
+  <Query query={GET_TODOS} pollInterval={45000}>
     {({loading, data, refetch}) => {
       return loading ? <h1>Loading...</h1> : <TodoList todos={data.todos} refetch={refetch}/>;
     }}
